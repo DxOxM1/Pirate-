730a6bcab07652063c7410aca1b60a140c723de0
DxOxM1 <86674391+DxOxM1@users.noreply.github.com>
2021-07-15 11:33:02 -0400

    Add files via upload

---
+++ js/CannonBall.js
@@ -0,0 +1,29 @@
+class CannonBall {
+    constructor(x, y) {
+      var options = {
+        isStatic: true,
+        restitution: 0.8
+      };
+      this.r = 40
+      this.body = Bodies.circle(x, y, this.r, options);
+      this.image= loadImage("assets/cannonball.png")
+      World.add(world, this.body);
+    }
+    display() {
+      var pos = this.body.position;
+      push()
+      translate(pos.x, pos.y);
+        rotate(this.body.angle);
+     imageMode(CENTER);
+      image(this.image,0,0, this.r, this.r);
+      pop();
+    }
+    shoot() {
+        //converting angle into x and y point
+        var velocity = p5.Vector.fromAngle(cannon.angle);
+        //speed
+        velocity.mult(20);
+        Matter.Body.setStatic(this.body, false);
+        Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
+      }
+  }
